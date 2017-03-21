309737c7281601c2ea0f05f92523bab55e76c8cf
Wilson Carey <carey@unif.io>
2017-03-21 11:44:30 -0400

    Updated examples and tests

---
+++ examples/complete/variables.tf
@@ -0,0 +1,116 @@
+# Input Variables
+
+## Resource tags
+variable "stack_item_label" {
+  type = "string"
+}
+
+variable "stack_item_fullname" {
+  type = "string"
+}
+
+variable "env_tag" {
+  type    = "string"
+  default = ""
+}
+
+variable "team_tag" {
+  type    = "string"
+  default = ""
+}
+
+## VPC parameters
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
+variable "subnets" {
+  type = "string"
+}
+
+## LC parameters
+variable "ami" {
+  type = "string"
+}
+
+variable "ebs_vol_device_name" {
+  type = "string"
+}
+
+variable "ebs_vol_encrypted" {
+  type    = "string"
+  default = ""
+}
+
+variable "ebs_vol_size" {
+  type    = "string"
+  default = ""
+}
+
+variable "ebs_vol_snapshot_id" {
+  type    = "string"
+  default = ""
+}
+
+variable "enable_monitoring" {
+  type    = "string"
+  default = ""
+}
+
+variable "instance_based_naming_enabled" {
+  type    = "string"
+  default = ""
+}
+
+variable "instance_name_prefix" {
+  type    = "string"
+  default = ""
+}
+
+variable "instance_type" {
+  type = "string"
+}
+
+variable "key_name" {
+  type    = "string"
+  default = ""
+}
+
+variable "root_vol_size" {
+  type    = "string"
+  default = ""
+}
+
+## ASG parameters
+variable "desired_capacity" {
+  type    = "string"
+  default = ""
+}
+
+variable "max_size" {
+  type = "string"
+}
+
+variable "min_elb_capacity" {
+  type    = "string"
+  default = ""
+}
+
+variable "min_size" {
+  type = "string"
+}
+
+variable "wait_for_elb_capacity" {
+  type    = "string"
+  default = ""
+}
+
+## ELB parameters
+variable "internal" {
+  type    = "string"
+  default = "true"
+}
--- examples/templates/user_data.tpl
+++ examples/templates/user_data.tpl
@@ -1,8 +1,4 @@
 #cloud-config
 runcmd:
-  - "export HOSTNAME=${hostname}-`curl -s http://169.254.169.254/latest/meta-data/instance-id | tr -d 'i-'`""
-  - "hostnamectl set-hostname $HOSTNAME"
-  - "echo $HOSTNAME > /var/lib/cloud/data/previous-hostname"
-  - "sed -i \"s/^127.0.0.1.*/127.0.0.1\ $HOSTNAME\ $HOSTNAME.${domain} localhost localhost.localdomain localhost4 localhost4.localdomain4/g\" /etc/hosts"
-  - "aws ec2 create-tags --region ${region} --resources `curl http://169.254.169.254/latest/meta-data/instance-id` --tags Key=Name,Value=`hostname`"
+  - yum install -y aws-cli
 output : { all : '| tee -a /var/log/cloud-init-output.log' }
